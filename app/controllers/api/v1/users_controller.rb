d34cccdf90515a718434c81b97b453a44e27f6ce
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-12-05 21:06:46 +0800

    Add Figaro - Protect my secret data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,60 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  def index
-    @users = policy_scope(User)
-  end
+ respond_to :json
+ acts_as_token_authentication_handler_for User, except: [:create ]
+
+ before_action :set_user, only: []
+
+ after_action :verify_authorized, except: [:create]
+
+
+ URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+
+
+ def create
+
+   # Send code, APPID and SECRET to weixin for openid and session_key
+   @user = User.find_by_email(wechat_email) || User.create(user_params)
+
+   render json: @user if @user.persisted?
+
+ end
+
+ private
+
+ def wechat_email
+   @wechat_email ||= wechat_user.fetch('openid')  + "@salmon.com"
+ end
+
+ def user_params
+   return @user_params if @user_params
+
+   @user_params = set_params
+
+   # GET both openid and session_key
+   @user_params['email'] = wechat_email
+   @user_params['password'] = wechat_user.fetch('session_key', Devise.friendly_token)
+   @user_params['authentication_token'] = Devise.friendly_token
+   @user_params
+ end
+
+ def wechat_user
+   @wechat_response ||= RestClient.post( URL, wechat_params )
+   @wechat_user ||= JSON.parse(@wechat_response.body)
+ end
+
+ def wechat_params
+   { appid: ENV.fetch('APPID'),
+     secret: ENV.fetch('SECRET'),
+     js_code: params[:code],
+     grant_type: "authorization_code" }
+ end
+
+ def set_user
+   @user = User.find(params[:id])
+   authorize @user  # For Pundit
+ end
+
+ def set_params
+   params.require(:user).permit(:nickname, :gender, :language, :avatar, :bio, :tag_list, :latitude, :longitude, :phone)
+ end
 end
