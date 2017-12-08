1f8b513b38d008313ea26d803149d4ea0ace96a7
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-12-08 14:34:11 +0800

    change schema that does not effect heroku

--- db/schema.rb
+++ db/schema.rb
@@ -24,15 +24,15 @@ ActiveRecord::Schema.define(version: 20171207051732) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer "price"
-    t.integer "discount"
+    t.string "price"
+    t.string "discount"
     t.string "description"
     t.string "image_url"
     t.string "category"
     t.boolean "is_private"
     t.integer "user_id"
-    t.integer "latitude"
-    t.integer "longitude"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
