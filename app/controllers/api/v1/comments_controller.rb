e991ad00d1b65d4569e03a95bde76acbc6e5406b
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-12-11 13:16:06 +0800

    remove sign-in from get all comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::CommentsController < Api::V1::BaseController
-  acts_as_token_authentication_handler_for User
+  acts_as_token_authentication_handler_for User, except: [:index ]
 
   def index
+    # curl -s http://localhost:3000/api/v1/items/:item_id/comments
     @comments = Comment.where(item_id: params[:item_id])
   end
 
   def create
+
+    # curl -i -X POST                                                              \
+    #  -H 'Content-Type: application/json'                                     \
+    #  -H 'X-User-Email: pitipon@gmail.com'                                      \
+    #  -H 'X-User-Token: n-TpyxLbRue6JATZQ77B'                                 \
+    #  -d '{ "comment": { "body": "Test comment"  } }' \
+    #  http://localhost:3000/api/v1/items/:item_id/comments
+
+
   @comment = Comment.new(comments_param)
   @comment.user = current_user
   @comment.item_id = params[:item_id]
