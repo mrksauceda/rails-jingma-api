48b5b627d397105a40b27e88717730a6275fd25b
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-12-12 12:29:35 +0800

    liked by current user

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -3,12 +3,24 @@ class Api::V1::LikesController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [:total_likes]
 
   def like
+    # curl -i -X POST                                                              \
+    #  -H 'Content-Type: application/json'                                     \
+    #  -H 'X-User-Email: pitipon@gmail.com'                                      \
+    #  -H 'X-User-Token: n-TpyxLbRue6JATZQ77B'                                 \
+    #  -d '{ "item_id": 1  }' \
+    #  http://localhost:3000/api/v1/likes/like
     @item = Item.find(params[:item_id])
     @item.liked_by current_user
     render json: {}, status: :ok
   end
 
   def unlike
+    # curl -i -X POST                                                              \
+    #  -H 'Content-Type: application/json'                                     \
+    #  -H 'X-User-Email: pitipon@gmail.com'                                      \
+    #  -H 'X-User-Token: n-TpyxLbRue6JATZQ77B'                                 \
+    #  -d '{ "item_id": 1  }' \
+    #  http://localhost:3000/api/v1/likes/unlike
     @item = Item.find(params[:item_id])
     @item.unliked_by current_user
     render json: {}, status: :ok
